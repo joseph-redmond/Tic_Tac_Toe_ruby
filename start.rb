f2538f5ba78f52ffc87906189d89edf86aa60d93
Joseph Redmond <josephredmond@Josephs-MacBook-Air.local>
2021-09-12 15:57:22 -0700

    removed the unneeded module declaration

--- start.rb
+++ start.rb
@@ -1,17 +1,15 @@
 require './Game'
-module App
-  player1_name = ''
-  player2_name = ''
-  while player1_name == ''
-    Game.clear_console
-    puts 'Player 1 please enter your name'
-    player1_name = gets.chomp
-  end
-  while player2_name == '' || player2_name == player1_name
-    Game.clear_console
-    puts 'Player 2 please enter your name'
-    player2_name = gets.chomp
-  end
-  game = Game.new({ name: player1_name, symbol: 'X' }, { name: player2_name, symbol: 'Y' })
-  game.start_game
+player1_name = ''
+player2_name = ''
+while player1_name == ''
+  Game.clear_console
+  puts 'Player 1 please enter your name'
+  player1_name = gets.chomp
 end
+while player2_name == '' || player2_name == player1_name
+  Game.clear_console
+  puts 'Player 2 please enter your name'
+  player2_name = gets.chomp
+end
+game = Game.new({ name: player1_name, symbol: 'X' }, { name: player2_name, symbol: 'Y' })
+game.start_game
